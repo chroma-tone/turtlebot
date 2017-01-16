a9c3525ee029f6b374f502d296c0eac752829868
Jeremy Gold <jeremy.gold@ikegps.com>
2017-01-16 22:51:47 +1300

    Initial implementation of Atom / PlatformIO REST API, and simple index.html to control

---
+++ src/BasicArduinoServer.cpp
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Servo.h>
+
+Servo myservo;
+
+const char* ssid = "IkeDev";
+const char* password = "onionr!ngs33";
+
+ESP8266WebServer server(80);
+
+const int servo_pin = 5;
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void handleLed(){
+  String response = "";
+  
+  if(server.args() > 0) {
+    // Assume arg 1 is servo_pin angle (in degrees)
+    String arg = server.arg(0);
+    int value = arg.toInt();
+    myservo.write(value);
+
+    response = "Servo pos: ";
+    response += arg;
+      
+    server.send(200, "text/plain", response.c_str());
+  }
+}
+
+void setup(void){
+  myservo.attach(servo_pin);
+  
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/led", handleLed);
+  
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
