5e9d09899256ca086ac60cf4d311cd90c3d998c6
Jeremy Gold <jeremy.gold@ikegps.com>
2017-01-23 12:46:43 +1300

    Renaming BasicArduinoServer.cpp to RobotControlServer.cpp

--- index_html_converter.py
+++ index_html_converter.py
@@ -3,8 +3,9 @@ Import("env")
 from subprocess import call
 
 def before_build(source, target, env):
-    print "### before_build ####"
+    print "##### before_build - Converting index.html to index_html.h #####\n"
     index_html_file = open("src/index_html.h", "w")
     call(["xxd", "-i", "web/index.html"], stdout = index_html_file)
+    index_html_file.close()
 
-env.AddPreAction("$BUILD_DIR/src/BasicArduinoServer.o", before_build)
+env.AddPreAction("$BUILD_DIR/src/RobotControlServer.o", before_build)
